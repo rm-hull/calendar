2949fbf0d2e6d51aa44129dee47949f5d5de23b7
Richard Hull <rm_hull@yahoo.co.uk>
2025-09-18 11:04:01 +0100

    Fix background bordering

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -4,14 +4,10 @@ import { useColorModeValue } from "./ui/color-mode";
 
 export default function Backdrop({ children }: PropsWithChildren) {
   return (
-    <Container
-      p={8}
-      width="100vw"
-      minHeight="100vh"
-      margin="0 auto"
-      background={useColorModeValue("orange.50", "gray.900")}
-    >
-      {children}
+    <Container fluid background={useColorModeValue("orange.50", "gray.900")}>
+      <Container p={8} width="100vw" minHeight="100vh" margin="0 auto">
+        {children}
+      </Container>
     </Container>
   );
 }

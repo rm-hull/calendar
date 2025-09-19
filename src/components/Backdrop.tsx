7524689fa265436bee9be58fa6dd5c0c161b3b5b
Richard Hull <rm_hull@yahoo.co.uk>
2025-09-19 23:01:57 +0100

    Further refinement of backdrop layout

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -4,8 +4,13 @@ import { useColorModeValue } from "./ui/color-mode";
 
 export default function Backdrop({ children }: PropsWithChildren) {
   return (
-    <Container fluid background={useColorModeValue("orange.50", "gray.900")}>
-      <Container p={8} width="100vw" minHeight="100vh" margin="0 auto">
+    <Container
+      fluid
+      background={useColorModeValue("orange.50", "gray.900")}
+      padding={0}
+      margin={0}
+    >
+      <Container p={6} pt={8} width="100vw" minHeight="100vh" margin="0 auto">
         {children}
       </Container>
     </Container>
